12211afa7ef3c18e05f45569503a395e0b94f6e9
Ryan <ryangroenewold@hotmail.com>
2022-09-01 14:00:13 +0200

    Made possible to filter the objects given by graphql

--- Avans Fysio WebService/GraphQL/Query.cs
+++ Avans Fysio WebService/GraphQL/Query.cs
@@ -2,25 +2,32 @@
 using Fysio_Codes.DAL;
 using HotChocolate;
 using HotChocolate.Types;
+using HotChocolate.Types.Filters;
 using Fysio_Codes.Models;
 using System.Linq;
 using Avans_Fysio_WebService.GraphQL.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Avans_Fysio_WebService.GraphQL
 {
     public class Query
     {
+        // Pagination if needed.
         //[UsePaging]
-        // Using this annotation we're essentially applying a middleware to the field resolver.
+        //Using this annotation we're essentially applying a middleware to the field resolver.
         [UseApplicationDbContext]
+        [UseFiltering]
         public Task<List<Diagnosis>> GetDiagnoses([ScopedService] FysioCodeDbContext context) =>
             context.Diagnoses.ToListAsync();
+        
+        // Pagination if needed.
         //[UsePaging]
         // Using this annotation we're essentially applying a middleware to the field resolver.
         [UseApplicationDbContext]
+        [UseFiltering]
         public Task<List<Treatment>> GetTreatments([ScopedService] FysioCodeDbContext context) =>
             context.Treatments.ToListAsync();
     }
