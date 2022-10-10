ecfd8ffa76d8c7d229232d0c18069551ceab046a
Ryan <ryangroenewold@hotmail.com>
2022-10-10 16:34:12 +0200

    Graphql by id works

--- Avans Fysio WebService/GraphQL/Query.cs
+++ Avans Fysio WebService/GraphQL/Query.cs
@@ -5,12 +5,14 @@ using HotChocolate;
 using HotChocolate.Types;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Avans_Fysio_WebService.GraphQL
 {
     public class Query
     {
+        
         // Pagination if needed.
         //[UsePaging]
         //Using this annotation we're essentially applying a middleware to the field resolver.
@@ -26,6 +28,19 @@ namespace Avans_Fysio_WebService.GraphQL
         [UseFiltering]
         public Task<List<Treatment>> GetTreatments([ScopedService] FysioCodeDbContext context) =>
             context.Treatments.ToListAsync();
+
+        [UseApplicationDbContext]
+        public Task<Treatment> GetTreatmentByCode([ScopedService] FysioCodeDbContext context, string code)
+        {
+            return context.Treatments.Where(treatment => treatment.Code == code).FirstOrDefaultAsync(); 
+        }
+
+        [UseApplicationDbContext]
+        public Task<Diagnosis> GetDiagnosesByCode([ScopedService] FysioCodeDbContext context, int id)
+        {
+            return context.Diagnoses.Where(diagnosis => diagnosis.Id == id).FirstOrDefaultAsync();
+        }
+
     }
 
 }
